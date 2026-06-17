9907e244817ffdb91c81fc6012f42ec05d015ae7
Snowflake-training <gespare@yahoo.com>
2026-06-16 17:10:01 -0700

    Correct variable name and display nutrition info
    Fix typo in variable name and update dataframe display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,11 +33,12 @@ ingredient_list = st.multiselect(
 )
 
 if ingredient_list:
-
     ingredients_string = ''    
 
     for fruit_chosen in ingredient_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen) 
         sf_df = st.dataframe(data=smoothiefroot_respose.json(), use_container_width=True)
 
   
