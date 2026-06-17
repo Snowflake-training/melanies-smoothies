f3cc17ec956b1bfe20329c704c40fbc95edf0125
Snowflake-training <gespare@yahoo.com>
2026-06-17 10:41:09 -0700

    Uncomment dataframe display and stop calls

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,8 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-#st.stop()
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 pd_pf = my_dataframe.to_pandas()
 st.dataframe(pd_pf)
@@ -42,7 +42,7 @@ if ingredient_list:
 
     for fruit_chosen in ingredient_list:
         ingredients_string += fruit_chosen + ' '
-
+      
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
       
