c62516725ec1628c10db68b7fd4c9af9df5bb5e7
Snowflake-training <gespare@yahoo.com>
2026-06-17 11:44:53 -0700

    Comment out st.stop() to prevent stopping execution

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ if ingredient_list:
       
         smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}") 
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-        st.stop()
+        #st.stop()
   
 
     st.write(ingredients_string)
