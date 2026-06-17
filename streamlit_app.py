2cfbe1276b31c4a6488be5b16ed230f4a4a34e58
Snowflake-training <gespare@yahoo.com>
2026-06-17 12:10:33 -0700

    Fix API endpoint to use search_on variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ if ingredient_list:
         #st.stop()
       
         st.subheader(fruit_chosen + ' Nutrition Information')      
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen) 
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}") 
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         st.stop()
   
